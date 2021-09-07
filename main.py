96d125655e00bbf1ec9a09ef183f8271ea9b93ce
Marcos Pereyra <MarkkesGames@hotmail.com>
2021-09-07 18:21:18 -0300

    segundo test de pull en main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import os
 import sqlite3
 
 #este mensaje es una prueba solo para saber si puedo clonar bien de repositorio local a otro local de otra pc 👉👈
+# este mensaje se una segunda prueba para ver si todo está en orden uwu
