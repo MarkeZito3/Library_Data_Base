46a43e285b5da8911e828500dd31eafd0dee0dc1
Marcos Pereyra <MarkkesGames@hotmail.com>
2021-09-08 16:16:19 -0300

    creo la function de la relacion entre students y inventory / I creat the function of the relation between students and inventory

--- dependences/main.py
+++ dependences/main.py
@@ -5,7 +5,7 @@ import sqlite3
 dir_of_dir = "carpeta"
 dir = "{}\database.db".format(dir_of_dir)
 
-def create_table_student(): #this function try to create a table "students" if this table doesn't exist
+def create_table_student(): #this function try to create a table "students" if this table doesn't already exist
     if not os.path.exists(dir_of_dir):
         os.makedirs(dir_of_dir)
     
@@ -28,7 +28,7 @@ def create_table_student(): #this function try to create a table "students" if t
         pass
     sql.close()
 
-def create_table_inventory():
+def create_table_inventory(): #this function try to create a table "inventory" if this table doesn't already exist
     if not os.path.exists(dir_of_dir):
         os.makedirs(dir_of_dir)
 
@@ -39,12 +39,30 @@ def create_table_inventory():
                         "author" text,
                         "editorial" text,
                         "category" text,
-                        "stock" integer NOT NULL
+                        "stock" integer NOT NULL,
+                        "item" text NOT NULL
                     );
         ''')
     except:
         pass
     sql.close()
 
-create_table_student()
-create_table_inventory()
+def create_table_borrowed(): #this function try to create a table of the relation of the tables "students" and "inventory" if this table doesn't already exist
+    if not os.path.exist(dir_of_dir):
+        os.makedirs(dir_of_dir)
+
+    sql = sqlite3.connect(dir)
+    try:
+        sql.execute("""
+                        CREATE TABLE `borrowed` (
+	                    `id_library_item_borrowed`	INTEGER,
+	                    `dni_student_borrowed`	INTEGER,
+	                    `date_out`	DATE,
+	                    `date_in`	DATE,
+	                    FOREIGN KEY(`id_library_item_borrowed`) REFERENCES `inventory`(`id_library_item`),
+	                    FOREIGN KEY(`dni_student_borrowed`) REFERENCES `student`(`dni_student`)
+                    );
+        """)
+    except:
+        pass
+    sql.close()
